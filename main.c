5c6a1c1af2020e2d14826159889e373e05fb4cfa
Douglas Ausech <dausech@gmail.com>
2010-08-01 17:10:58 -0300

    use a gramatica apropriada.

--- main.c
+++ main.c
@@ -10,6 +10,6 @@ int main(int argc, char **argv)
 	printf("length = %d\n", strlen(name)); /* debug line */
 	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
-	printf("Hello %s!\n", name);
+	printf("Hello, %s!\n", name);
 	return 0;
 }
